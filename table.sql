a06ef2837341a7d5137a516fdb4e4806e5153dc1
VVallerian <113937286+VVallerian@users.noreply.github.com>
2022-10-30 19:49:49 +0200

    My SQL

---
+++ table.sql
@@ -0,0 +1,13 @@
+insert into homework_24.users (no, username , first_name, second_name, age, email, password) 
+values ( 1,'Valok', 'Valeriia', 'Vornichesko', 22, 'leravornichesko.ru@gmail.com', '12345');
+
+select * from homework_24.users ; 
+
+insert into homework_24.users (no, username , first_name, second_name, age, email, password) 
+values 
+( 2,'John22', 'John', 'Brown', 32, 'johnbrown@gmail.com', '33444'),
+(3, 'Irenchik', 'Irene', 'Hall', 25, 'irenehall@gmail.com', '66775'),
+( 4,'Wils@n', 'Anatole', 'Wilson', 27, 'wilsonnnn@gmail.com', '8866'),
+( 5,'Poll', 'Paul', 'Moore', 29, 'mooreee@gmail.com', '665555'),
+( 6,'Thoma&', 'Helen', 'Thomas', 24, 'helenthom@gmail.com', '665544'),
+(7, 'Juli@', 'Julia', 'Taylor', 28, 'taylorju@gmail.com', '12345');
